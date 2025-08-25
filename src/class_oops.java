f30385cbad81ef2612d39d7483aff57044e4d4eb
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-08-25 17:23:17 +0530

    basic

--- src/class_oops.java
+++ src/class_oops.java
@@ -1,9 +1,17 @@
 public class class_oops {
+    void display () {
+        System.out.println("I am So Happy");
+        System.out.println("Love you bro");
+    }
 
     public static void main (String [] args)
     {
 
         System.out.println("Hello class");
+        class_oops T1= new class_oops();
+        T1.display();
+
+
     }
 
 
